7c53656183b6bf0cc13aa5e5f7035b2c4827d4b9
dexam <paulvanloon88@hotmail.com>
2014-08-27 15:15:50 +0100

    u

--- R/hello_script_1.R
+++ R/hello_script_1.R
@@ -1,7 +1,14 @@
-hello<-function(name=""){
-print(paste("Hello", name))
-
+#' Hello World
+#' 
+#' Basic hello world function to be called from the demo app
+#' 
+#' @export
+#' @param myname your name. Required.
+hello <- function(myname = ""){
+  if(myname == ""){
+    stop("Tell me yong!")
+  }
+  list(
+    message = paste("hello", myname, "start playing!")
+  )
 }
-
-
-
